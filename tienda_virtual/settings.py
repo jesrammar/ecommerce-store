80c9f45311478a14551b758ae6288d71c89b7fc8
jesrammar <jesrammar2@alumn.us.es>
2025-10-16 12:21:01 +0100

    git commit -m "feat: new login and register implemented!"

--- tienda_virtual/settings.py
+++ tienda_virtual/settings.py
@@ -75,10 +75,9 @@ TIME_ZONE='Europe/Madrid'
 USE_I18N=True
 USE_TZ=True
 
-STATIC_URL='/static/'
-# STATICFILES_DIRS = [BASE_DIR/'static']  # opcional en dev
-STATIC_ROOT = BASE_DIR/'staticfiles'
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"  # en prod
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']      # 
+
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
